162470bd4b53f4d8dd62b06cfd254ad24245d1d6
mercicle <presci3nce@gmail.com>
2018-05-04 13:53:40 -0400

    - refactored some - made squares editable - moved state up to Board

--- src/index.js
+++ src/index.js
@@ -10,45 +10,96 @@ import d3 from 'd3';
 import _ from 'underscore';
 
 const matrix_rank = 2;
+const total_letters = matrix_rank * matrix_rank;
+const space = _.range(matrix_rank);
+
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
 
 const get_random_letter = (in_alphabet) => {
 	return in_alphabet[_.random(0, in_alphabet.length-1)]
 }
 
+const get_random_board = (size) => {
+	return  _.map(_.range(size), function(){return get_random_letter(alphabet)})
+}
+
 class Square extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // thanks to
+  //https://stackoverflow.com/questions/38394015/how-to-pass-data-from-child-component-to-its-parent-in-reactjs
+
+  handleChange(event) {
+     this.props.sendData(this.props.id, event.target.value);
+  }
+ 
   render() {
     return (
-      <input className="square" type="text" value={this.props.value}></input>
+      <input className="square" 
+              type="text" 
+              name="square"
+              value={this.props.value} 
+              onChange={ this.handleChange }> 
+      </input>
     );
   }
 }
 
 class Board extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: get_random_board(total_letters),
+    };
+
+    this.renderSquare = this.renderSquare.bind(this);
+    this.getData = this.getData.bind(this);
+
+  }
+
+  getData(i, new_letter){
+	    const squares = this.state.squares.slice(); // copy to respect immutability
+	    squares[i] = new_letter;
+	    this.setState({squares: squares});
+  }
+ 
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square value={this.state.squares[i]} 
+        	  key = {i} 
+        	  id = {i} // for ease in Square handleChange
+        	  sendData={this.getData} />
+    );
   }
 
   render() {
 
-    const status = 'Next player: X';
-    const space = _.range(matrix_rank);
-    var cell_index = 0 ;
-
+    const status = 'Unsolved';
+  
     return (
       <div className="boardContainer">
         <div className="status">{status}</div>
+        { /* Iteration through rows and append Square column elements 
+             dynamically based on global parms */ 
+        }
         {space.map(function(i){
-        	return <div className="board-row"> 
+        	 
+        	return <div className="board-row" key={ "r"+i}> 
         			{space.map(function(j){ 
-        				cell_index +=1;
-        				var temp_char = get_random_letter(alphabet)
-        				return <Square value={temp_char} key = {cell_index} />; })
+        				  
+        				var temp_vector_index  =  i * matrix_rank + j;
+        				return this.renderSquare(temp_vector_index)
+
+        			  }, this)
         		     }
         		    </div>
         	 
-         })}
+         }, this)}
         
       </div>
     );
